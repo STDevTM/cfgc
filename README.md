d6dfc9625fdb24f5e67b883f030f81d5964c52ad
Tigran Hambardzumyan <tigran@stdevmail.com>
2018-04-30 16:46:45 +0400

    Adding initial codes

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# cfgc
-Changelog from git commits
+# CFGC - Changelog from git commits
+
+## Overview
+
+This VSTS/TFS marketplace task generates changelog from commit messages. This task was built inspired form [changelog_from_git_commits](https://docs.fastlane.tools/actions/changelog_from_git_commits/) action of [Fastlane](https://fastlane.tools/) tools.
+
+![cfgc options](./images/options.png)
+
+## Options
+
+The task has 2 options to generate changelog:
+
+* **Since last tag** - Generate changelog from git commit messages since last tag in git log,
+* **Between Revisions** - Specify range of revisions from where changelog will be generated
+  * **From revision** - Tag name or revision hash of start commit.
+  * **To revision** - Tag name or revision hash of last commit.
+
+![beetwen option](./images/between-option.png)
+
+## Customization
+
+* **Pretty** - The format applied to each commit while generating the collected value.
+* **Date format** - The date format applied to each commit while generating the collected value.
+* **Include Merges** - Whether or not to include any commits that are merges.
+
+## Author
+
+Tigran Hambardzumyan, tigran@stdevmail.com
+
+## Support
+
+Feel free to [open issuses](https://github.com/stdevteam/cfgc/issues/new) with any suggestions, bug reports, feature requests, questions.
+
+## Let us know!
+
+We’d be really happy if you sent us links to your projects where you use our component. Just send an email to developer@stdevmail.com and do let us know if you have any questions or suggestion.
+
+## License
+
+RxRestClient is available under the MIT license. See the [LICENSE](./LICENSE) file for more info.
---
+++ cfgc/index.ts
@@ -0,0 +1,96 @@
+import * as tl from "vsts-task-lib/task";
+import { IExecOptions, IExecSyncResult, ToolRunner } from 'vsts-task-lib/toolrunner';
+
+enum Options {
+    LastTag = "last-tag",
+    Between = "between"
+}
+
+class ChangelogGenerator {
+
+    private fromRev: string = "";
+    private toRev: string = "HEAD";
+    private pretty: string = "";
+    private includeMerges: boolean = false;
+    private dateFormat: string = "";
+
+    private workingFolder: string;
+
+    constructor() {
+        this.workingFolder = tl.getVariable('System.DefaultWorkingDirectory');
+        console.log(this.workingFolder);
+        let option = tl.getInput("option", true) as Options;
+        switch (option) {
+            case Options.LastTag: {
+                this.fromRev = this.getLastTag();
+                this.toRev = "HEAD";
+            }
+            break;
+            case Options.Between: {
+                this.fromRev = tl.getInput("from_rev", true);
+                this.toRev = tl.getInput("to_rev", true);
+            }
+            break;
+        }
+        this.pretty = tl.getInput("pretty", false);
+        this.dateFormat = tl.getInput("date_format", false);
+        this.includeMerges = tl.getBoolInput("include_merges", true);
+    }
+
+    public async run() {
+        try {
+            tl.setVariable("CHANGELOG", this.getChangelog());
+            tl.setResult(tl.TaskResult.Succeeded, "Changelog was generated and saved to $(<Task>.CHANGELOG) variable");
+        } catch (err) {
+            tl.setResult(tl.TaskResult.Failed, tl.loc('TaskFailed', err.message));
+        }
+    }
+
+    private getChangelog(): string {
+        let args = ["-C", this.workingFolder, "log", this.fromRev + ".." + this.toRev];
+        if (!this.includeMerges) {
+            args.push("--no-merges");
+        }
+        if (this.dateFormat !== null && this.dateFormat !== "") {
+            args.push(`--date=${this.dateFormat}`);
+        }
+        if (this.pretty !== null && this.pretty !== "") {
+            args.push(`--pretty=${this.pretty}`);
+        }
+        let res = this.execGit(args);
+        if (res.code !== 0) {
+            tl.error(`Could not get changelog: ${res.stderr}`);
+        }
+        return res.stdout;
+    }
+
+    private getLastTag(): string {
+        let res = this.execGit(["-C", this.workingFolder, "describe", "--abbrev=0", "--tags"]);
+        if (res.code !== 0) {
+            tl.error(`Could not get last tag: ${res.stderr}`);
+        }
+        return res.stdout.trim();
+    }
+
+    execGit(gitArgs: string[], options?: IExecOptions): IExecSyncResult {
+        let gitTool =  new ToolRunner(tl.which("git", true));
+        gitTool.arg(gitArgs);
+        options = options || <IExecOptions>{};
+        try {
+            let obj = gitTool.execSync(options);
+            return obj;
+        }
+        catch (err) {
+            tl.debug('execGit failed');
+            tl.setResult(tl.TaskResult.Failed, tl.loc('GitFailed', err.message));
+        }
+        return;
+    }
+}
+
+async function run() {
+    let generator = new ChangelogGenerator();
+    await generator.run();
+}
+
+run();
